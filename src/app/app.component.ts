3feac40d407f71c627bbdafa5967411650e6cfde
Ljojua15 <jojualasha15@gmail.com>
2024-10-29 22:18:06 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlipCardsComponent } from "./flip-cards/flip-cards.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlipCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'reservation-app';
 }
