f4f63e274e4fff965229df8d46bbf29b94c90a91
Ljojua15 <jojualasha15@gmail.com>
2024-10-30 11:02:34 +0400

    'slider'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FlipCardsComponent } from "./flip-cards/flip-cards.component";
+import { FlipCardsComponent } from './flip-cards/flip-cards.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FlipCardsComponent],
+  imports: [RouterOutlet, FlipCardsComponent, SliderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
