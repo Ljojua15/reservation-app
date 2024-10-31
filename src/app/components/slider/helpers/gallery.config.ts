ff2666be0af119aa6880c93d9af3507314b88b63
Ljojua15 <jojualasha15@gmail.com>
2024-10-31 11:24:03 +0400

    'swiper'

---
+++ src/app/components/slider/helpers/gallery.config.ts
@@ -0,0 +1,24 @@
+import { Gallery } from './gallery';
+
+const galleryUrl: string = './assets/cyber-popups/gallery/';
+
+export const galleryConfig: Gallery[] = [
+  {
+    imgSrc: `${galleryUrl}1.png`,
+  },
+  {
+    imgSrc: `${galleryUrl}2.png`,
+  },
+  {
+    imgSrc: `${galleryUrl}3.png`,
+  },
+  {
+    imgSrc: `${galleryUrl}4.png`,
+  },
+  {
+    imgSrc: `${galleryUrl}5.png`,
+  },
+  {
+    imgSrc: `${galleryUrl}6.png`,
+  },
+];
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { galleryConfig } from './helpers/gallery.config';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
 export class SliderComponent {
+  protected readonly autoConfig = galleryConfig;
 
+  selectedIndex = 0;
+
+  showPrev(i: number) {
+    console.log('left');
+    if (this.selectedIndex > 0) {
+      this.selectedIndex = i - 1;
+      console.log('left-in');
+    }
+  }
+  showNext(i: number) {
+    console.log('right');
+    if (this.selectedIndex < this.autoConfig.length - 1) {
+      this.selectedIndex = i + 1;
+      console.log('right-in');
+    }
+  }
 }
