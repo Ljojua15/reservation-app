7a0c3639a74b8d74c6fabff35c2fd623b29d2b23
ljojua <ljojua@crocobet.com>
2024-12-12 14:57:18 +0400

    swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlipCardsComponent } from './flip-cards/flip-cards.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { SwiperComponent } from './components/swiper/swiper.component';
+import { InputsComponent } from './components/inputs/inputs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FlipCardsComponent, SliderComponent],
+  imports: [RouterOutlet, FlipCardsComponent, SliderComponent, SwiperComponent, InputsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild, viewChild } from '@angular/core';
 import {NgForOf} from "@angular/common";
 
 @Component({
@@ -10,11 +10,69 @@ import {NgForOf} from "@angular/common";
   templateUrl: './swiper.component.html',
   styleUrl: './swiper.component.scss'
 })
-export class SwiperComponent {
+export class SwiperComponent implements AfterViewInit {
+
+  @ViewChild('main') elementReference!: ElementRef
+
+
+  private scrollArrow: number = 50 
+  private scrollMouse: number = 30 
+  private   isDragging = false;  
   startDate = new Date();
   endDate = new Date();
+  startX!: number;
+  scrollLeft!: any;
   constructor() {
-    console.log(this.startDate);
   }
 
+
+  ngAfterViewInit(): void {
+
+  }
+
+
+  rightClick(){
+    this.elementReference.nativeElement.scrollLeft += this.scrollArrow;
+  }
+
+ leftClick(){
+  this.elementReference.nativeElement.scrollLeft -=  this.scrollArrow;
+  }
+
+  @HostListener('wheel', ['$event'])
+  onWheel(event: WheelEvent): void {
+    event.preventDefault();
+
+    if (event.deltaY > 0) {
+      this.elementReference.nativeElement.scrollLeft +=  this.scrollMouse; 
+    } else {
+      this.elementReference.nativeElement.scrollLeft -= this.scrollMouse;; 
+    }
+  }
+  onMouseDown(event: MouseEvent): void {
+    this.isDragging = true;
+    this.startX = event.pageX - this.elementReference.nativeElement.offsetLeft;
+    this.scrollLeft = this.elementReference.nativeElement.scrollLeft;
+    event.preventDefault();  // Prevent default drag behavior
+  }
+
+  // While dragging the swiper (mouse move)
+  onMouseMove(event: MouseEvent): void {
+    if (!this.isDragging) return;  // If not dragging, ignore
+
+    const x = event.pageX - this.elementReference.nativeElement.offsetLeft;
+    const scroll = x - this.startX;
+    this.elementReference.nativeElement.scrollLeft = this.scrollLeft - scroll;  // Adjust the scroll position
+  }
+
+  // Stop dragging the swiper (mouse up or leave)
+  onMouseUp(): void {
+    this.isDragging = false;
+  }
+
+  // Stop dragging when the mouse leaves the swiper container
+  onMouseLeave(): void {
+    this.isDragging = false;
+  }
 }
+
