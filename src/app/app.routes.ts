3eb8294c744be7ecc43d4889cf267781a0067912
ljojua <ljojua@crocobet.com>
2024-12-19 10:55:21 +0400

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { RecoverComponent } from './components/auth/recover/recover.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login' , pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'recover',
+        component: RecoverComponent
+    }
+
+];
